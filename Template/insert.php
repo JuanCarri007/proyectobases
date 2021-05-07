52dea46868107952ccc652989e41d84b63635902
JuanCarri007 <71296301+JuanCarri007@users.noreply.github.com>
2021-05-07 10:06:24 -0500

    Client form

---
+++ Template/insert.php
@@ -0,0 +1,9 @@
+<?php
+    $id_client = $_POST['id_client'];
+    $name_client = $_POST['contact_name'];
+    $birth_date = $_POST['birth_date'];
+
+    $host = "localhost";
+    $dbusername = "root";
+    $dbpassword = "";
+?>
